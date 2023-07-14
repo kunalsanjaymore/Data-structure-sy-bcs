4e49fa29d420fd80d9e417eebe6d7b979283a602
Kunal More <138104970+kunalsanjaymore@users.noreply.github.com>
2023-07-14 18:36:03 +0530

    Add files via upload

---
+++ linked list function.c
@@ -0,0 +1,48 @@
+
+#include<stdlib.h>
+
+struct node
+{
+  int data;
+  struct node *next;
+};
+struct node* create(struct node *head)
+{
+  int n,i;
+  struct node *newnode,*temp;
+  printf("enter limit");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+{
+  newnode=(struct node*)malloc(sizeof(struct node));
+  printf("enter value");
+  scanf("%d",&newnode->data);
+  newnode->next=NULL;
+ if(head==NULL)
+{
+  head=newnode;
+  temp=newnode;
+}
+ else
+{
+  temp->next=newnode;
+ temp=newnode;
+}
+}
+return head;
+}
+
+ void disp(struct node *head)
+{
+  struct node *temp;
+  for(temp=head;temp!=NULL;temp=temp->next)
+ {
+   printf("%d",temp->data);
+}
+}
+int main()
+{
+  struct node *head=NULL;
+  head=create(head);
+  disp(head);
+}
