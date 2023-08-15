65621b665bccd6bd1ac5de709dad4f76a5bbc431
Kunal More <138104970+kunalsanjaymore@users.noreply.github.com>
2023-08-15 10:30:53 +0530

    Add files via upload

---
+++ bubble sort function.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+  int a[100],n,i;
+  void insertion(int a[100],int n);
+ printf("enter limit");
+ scanf("%d",&n);
+ printf("enter n number");
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+}
+insertion(a,n);
+}
+
+void insertion(int a[100],int n)
+{
+  int i,j,num,t;
+ for(i=1;i<n;i++)
+{ 
+  num=a[i];
+ for(j=i-1;j>=0&&num<a[j];j--)
+{
+   t=a[j+1];
+   a[j+1]=a[j];
+  a[j]=t;
+}
+}
+
+printf("sorted no= ");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+}
+
+
